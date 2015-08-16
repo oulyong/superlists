b98c0d90fbe6c5c7fde280768a032486b5d078c2
oulyong <civilrevolutionl@gmail.com>
2015-08-16 10:59:33 +0800

    split url to lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,15 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
+urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list',
-        name='view_list'
-    ),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', 
-        name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^lists/', include('lists.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
-]
+)
